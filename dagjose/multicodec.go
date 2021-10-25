33dfe9d4df3e9e00d83efc02b87bea18e0648af6
Mohsin Zaidi <2236875+smrz2001@users.noreply.github.com>
2021-10-25 14:59:50 -0400

    Add fast-path to Encoding

--- dagjose/multicodec.go
+++ dagjose/multicodec.go
@@ -2,6 +2,7 @@ package dagjose
 
 import (
 	"io"
+	"reflect"
 
 	"github.com/ipld/go-ipld-prime/codec/cbor"
 	"github.com/ipld/go-ipld-prime/datamodel"
@@ -17,16 +18,22 @@ func Decode(na datamodel.NodeAssembler, r io.Reader) error {
 // Encode walks the given datamodel.Node and serializes it to the given
 // io.Writer. Encode fits the codec.Encoder function interface.
 func Encode(n datamodel.Node, w io.Writer) error {
-	// Use `datamodel.Copy` to convert the passed `Node` into a `dagJOSENode`
-	// via `dagJOSENodeBuilder`, which applies all the necessary validations
-	// required to construct a proper DAG-JOSE IPLD Node.
-	dagJoseBuilder := NewBuilder()
-	err := datamodel.Copy(n, dagJoseBuilder)
-	if err != nil {
-		return err
+	// If the passed `datamodel.Node` is already of type `dagjose.dagJOSENode`,
+	// skip conversion to the latter type.
+	if reflect.TypeOf(n) != reflect.TypeOf(new(dagJOSENode)) {
+		// Use `datamodel.Copy` to convert the passed `datamodel.Node` into a
+		// `dagjose.dagJOSENode` via `dagjose.dagJOSENodeBuilder`, which applies
+		// all the necessary validations required to construct a proper DAG-JOSE
+		// IPLD Node.
+		dagJoseBuilder := NewBuilder()
+		err := datamodel.Copy(n, dagJoseBuilder)
+		if err != nil {
+			return err
+		}
+		n = dagJoseBuilder.Build()
 	}
 	// CBOR is a superset of DAG-JOSE and, as such, can be used to encode valid
 	// DAG-JOSE objects without encoding the CID (as expected by the DAG-JOSE
 	// spec: https://specs.ipld.io/block-layer/codecs/dag-jose.html).
-	return cbor.Encode(dagJoseBuilder.Build(), w)
+	return cbor.Encode(n, w)
 }
