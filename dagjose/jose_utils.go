f1e15656fa3c4b1d9fa75cf87d018a2136532789
Mohsin Zaidi <2236875+smrz2001@users.noreply.github.com>
2021-11-15 14:20:38 -0500

    support for "flattened" JOSE objects

---
+++ dagjose/jose_utils.go
@@ -0,0 +1,245 @@
+package dagjose
+
+import (
+	"bytes"
+	"errors"
+	ipldJson "github.com/ipld/go-ipld-prime/codec/dagjson"
+	"github.com/ipld/go-ipld-prime/datamodel"
+	"github.com/ipld/go-ipld-prime/node/basicnode"
+	"gopkg.in/square/go-jose.v2/json"
+	"reflect"
+)
+
+func unflattenJOSE(n datamodel.Node) (datamodel.Node, error) {
+	// "flattened" fields are not included in the schema and thus never encoded. That means that this cannot have been
+	// called on a JOSE-related node because we wouldn't have gotten this far without an error have occurred earlier.
+	// We'll assume this is some sort of Map-type node that we can reconstruct to be in a "general" form before any
+	// actual JOSE-related operations are performed on it.
+	if jwe, err := isJWE(n); err != nil {
+		return nil, err
+	} else if jwe {
+		return unflattenJWE(n)
+	} else if jws, err := isJWS(n); err != nil {
+		return nil, err
+	} else if jws {
+		return unflattenJWS(n)
+	} else {
+		return nil, errors.New("invalid JOSE object")
+	}
+}
+
+func unflattenJWE(n datamodel.Node) (datamodel.Node, error) {
+	if recipients, err := lookupIgnoreAbsent("recipients", n); err != nil {
+		return nil, err
+	} else if recipients != nil {
+		// If `recipients` is present, this must be a "general" JWE and no changes are needed.
+		return n, nil
+	} else
+	// If `recipients` is absent, this must be a "flattened" JWE.
+	if ciphertext, err := n.LookupByString("ciphertext"); err != nil {
+		// `ciphertext` is mandatory so if any error occurs, return from here
+		return nil, err
+	} else if ciphertextString, err := ciphertext.AsString(); err != nil {
+		return nil, err
+	} else {
+		recipients := make([]map[string]interface{}, 1)
+		recipients[0] = make(map[string]interface{}, 0) // all recipient fields are optional
+		jwe := map[string]interface{}{
+			"ciphertext": ciphertextString,
+		}
+		if aad, err := lookupIgnoreAbsent("aad", n); err != nil {
+			return nil, err
+		} else if aad != nil {
+			if aadString, err := aad.AsString(); err != nil {
+				return nil, err
+			} else {
+				jwe["aad"] = aadString
+			}
+		}
+		if encryptedKey, err := lookupIgnoreAbsent("encrypted_key", n); err != nil {
+			return nil, err
+		} else if encryptedKey != nil {
+			if encryptedKeyString, err := encryptedKey.AsString(); err != nil {
+				return nil, err
+			} else {
+				recipients[0]["encrypted_key"] = encryptedKeyString
+			}
+		}
+		if header, err := lookupIgnoreAbsent("header", n); err != nil {
+			return nil, err
+		} else if header != nil {
+			if headerMap, err := nodeToMap(header); err != nil {
+				return nil, err
+			} else {
+				recipients[0]["header"] = headerMap
+			}
+		}
+		if iv, err := lookupIgnoreAbsent("iv", n); err != nil {
+			return nil, err
+		} else if iv != nil {
+			if ivString, err := iv.AsString(); err != nil {
+				return nil, err
+			} else {
+				jwe["iv"] = ivString
+			}
+		}
+		if protected, err := lookupIgnoreAbsent("protected", n); err != nil {
+			return nil, err
+		} else if protected != nil {
+			if protectedString, err := protected.AsString(); err != nil {
+				return nil, err
+			} else {
+				jwe["protected"] = protectedString
+			}
+		}
+		if tag, err := lookupIgnoreAbsent("tag", n); err != nil {
+			return nil, err
+		} else if tag != nil {
+			if tagString, err := tag.AsString(); err != nil {
+				return nil, err
+			} else {
+				jwe["tag"] = tagString
+			}
+		}
+		if unprotected, err := lookupIgnoreAbsent("unprotected", n); err != nil {
+			return nil, err
+		} else if unprotected != nil {
+			if unprotectedMap, err := nodeToMap(unprotected); err != nil {
+				return nil, err
+			} else {
+				jwe["unprotected"] = unprotectedMap
+			}
+		}
+		// Only add `recipients` to the JWE if one or more fields were present in the map
+		if len(recipients[0]) > 0 {
+			jwe["recipients"] = recipients
+		}
+		return mapToNode(jwe)
+	}
+}
+
+func unflattenJWS(n datamodel.Node) (datamodel.Node, error) {
+	if signatures, err := lookupIgnoreAbsent("signatures", n); err != nil {
+		return nil, err
+	} else if signatures != nil {
+		// If `signatures` is present, this must be a "general" JWS and no changes are needed.
+		return n, nil
+	} else
+	// If `signatures` is absent, this must be a "flattened" JWS.
+	if payload, err := n.LookupByString("payload"); err != nil {
+		// `payload` is mandatory so if any error occurs, return from here
+		return nil, err
+	} else if payloadString, err := payload.AsString(); err != nil {
+		return nil, err
+	} else {
+		signatures := make([]map[string]interface{}, 1)
+		signatures[0] = make(map[string]interface{}, 1) // at least `signature` must be present
+		jws := map[string]interface{}{
+			"payload":    payloadString,
+			"signatures": signatures,
+		}
+		if header, err := lookupIgnoreAbsent("header", n); err != nil {
+			return nil, err
+		} else if header != nil {
+			if headerMap, err := nodeToMap(header); err != nil {
+				return nil, err
+			} else {
+				signatures[0]["header"] = headerMap
+			}
+		}
+		if protected, err := lookupIgnoreAbsent("protected", n); err != nil {
+			return nil, err
+		} else if protected != nil {
+			if protectedString, err := protected.AsString(); err != nil {
+				return nil, err
+			} else {
+				signatures[0]["protected"] = protectedString
+			}
+		}
+		if signature, err := lookupIgnoreAbsent("signature", n); err != nil {
+			return nil, err
+		} else if signature != nil {
+			if signatureString, err := signature.AsString(); err != nil {
+				return nil, err
+			} else {
+				signatures[0]["signature"] = signatureString
+			}
+		}
+		return mapToNode(jws)
+	}
+}
+
+func isJWS(n datamodel.Node) (bool, error) {
+	if payload, err := lookupIgnoreAbsent("payload", n); err != nil {
+		return false, err
+	} else {
+		return payload != nil, nil
+	}
+}
+
+func isJWE(n datamodel.Node) (bool, error) {
+	if ciphertext, err := lookupIgnoreAbsent("ciphertext", n); err != nil {
+		return false, err
+	} else {
+		return ciphertext != nil, nil
+	}
+}
+
+func mapToNode(m map[string]interface{}) (datamodel.Node, error) {
+	if jsonBytes, err := json.Marshal(m); err != nil {
+		return nil, err
+	} else {
+		na := basicnode.Prototype.Any.NewBuilder()
+		if err := ipldJson.Decode(na, bytes.NewReader(jsonBytes)); err != nil {
+			return nil, err
+		} else {
+			return na.Build(), nil
+		}
+	}
+}
+
+func nodeToMap(n datamodel.Node) (map[string]interface{}, error) {
+	jsonBytes := bytes.NewBuffer([]byte{})
+	if err := (ipldJson.EncodeOptions{
+		EncodeLinks: false,
+		EncodeBytes: false,
+	}.Encode(n, jsonBytes)); err != nil {
+		return nil, err
+	} else {
+		m := map[string]interface{}{}
+		if err := json.Unmarshal(jsonBytes.Bytes(), &m); err != nil {
+			return nil, err
+		} else {
+			return sanitizeMap(m), nil
+		}
+	}
+}
+
+// Remove all `nil` values from the top-level structure or from within nested maps or slices
+func sanitizeMap(m map[string]interface{}) map[string]interface{} {
+	for key, value := range m {
+		if value == nil {
+			delete(m, key)
+		} else if reflect.ValueOf(value).Kind() == reflect.Slice {
+			for idx, entry := range value.([]interface{}) {
+				if reflect.ValueOf(entry).Kind() == reflect.Map {
+					m[key].([]interface{})[idx] = sanitizeMap(m[key].([]interface{})[idx].(map[string]interface{}))
+				}
+			}
+		} else if reflect.ValueOf(value).Kind() == reflect.Map {
+			m[key] = sanitizeMap(value.(map[string]interface{}))
+		}
+	}
+	return m
+}
+
+func lookupIgnoreAbsent(key string, n datamodel.Node) (datamodel.Node, error) {
+	if value, err := n.LookupByString(key); err != nil {
+		if _, notFoundErr := err.(datamodel.ErrNotExists); !notFoundErr {
+			return nil, err
+		}
+		return nil, nil
+	} else {
+		return value, nil
+	}
+}
--- dagjose/multicodec.go
+++ dagjose/multicodec.go
@@ -18,13 +18,21 @@ import (
 )
 
 func init() {
+	// Always add `link` during default decode if `payload` is present
 	multicodec.RegisterDecoder(0x85, Decode)
 	multicodec.RegisterEncoder(0x85, Encode)
 }
 
+// DecodeOptions can be used to customize the behavior of a decoding function. The Decode method on this struct fits the
+// codec.Decoder function interface.
+type DecodeOptions struct {
+	// If true and the `payload` field is present, add a `link` field corresponding to the `payload`.
+	AddLink bool
+}
+
 // Decode deserializes data from the given io.Reader and feeds it into the given datamodel.NodeAssembler. Decode fits
 // the codec.Decoder function interface.
-func Decode(na datamodel.NodeAssembler, r io.Reader) error {
+func (cfg DecodeOptions) Decode(na datamodel.NodeAssembler, r io.Reader) error {
 	// If the passed `NodeAssembler` is not of type `_DecodedJOSE__ReprBuilder`, create and use a
 	// `_DecodedJOSE__ReprBuilder`.
 	joseBuilder, alreadyJose := na.(*_DecodedJOSE__ReprBuilder)
@@ -33,21 +41,22 @@ func Decode(na datamodel.NodeAssembler, r io.Reader) error {
 	}
 	// DAG-CBOR is a superset of DAG-JOSE and can be used to decode valid DAG-JOSE objects. Use DAG-CBOR decoding but do
 	// not allow IPLD Links. See: https://specs.ipld.io/block-layer/codecs/dag-jose.html
-	err := dagcbor.DecodeOptions{
+	if err := (dagcbor.DecodeOptions{
 		AllowLinks: false,
-	}.Decode(joseBuilder, r)
-	if err != nil {
+	}.Decode(joseBuilder, r)); err != nil {
 		return err
 	}
-	// If `payload` is present but `link` is not, add `link` with the corresponding encoded CID.
-	payloadNode := &joseBuilder.w.payload
-	linkNode := &joseBuilder.w.link
-	if payloadNode.Exists() && !linkNode.Exists() {
-		if link, err := Type.Base64Url.Link(&payloadNode.v); err != nil {
-			return err
-		} else {
-			linkNode.m = schema.Maybe_Value
-			linkNode.v = *link
+	if cfg.AddLink {
+		// If `payload` is present but `link` is not, add `link` with the corresponding encoded CID.
+		payloadNode := &joseBuilder.w.payload
+		linkNode := &joseBuilder.w.link
+		if payloadNode.Exists() && !linkNode.Exists() {
+			if link, err := Type.Base64Url.Link(&payloadNode.v); err != nil {
+				return err
+			} else {
+				linkNode.m = schema.Maybe_Value
+				linkNode.v = *link
+			}
 		}
 	}
 	// The "representation" node gives an accurate view of fields that are actually present
@@ -60,10 +69,18 @@ func Decode(na datamodel.NodeAssembler, r io.Reader) error {
 	return nil
 }
 
+// Decode deserializes data from the given io.Reader and feeds it into the given datamodel.NodeAssembler. Decode fits
+// the codec.Decoder function interface.
+func Decode(na datamodel.NodeAssembler, r io.Reader) error {
+	return DecodeOptions{
+		AddLink: true,
+	}.Decode(na, r)
+}
+
 // Encode walks the given datamodel.Node and serializes it to the given io.Writer. Encode fits the codec.Encoder
 // function interface.
 func Encode(n datamodel.Node, w io.Writer) error {
-	if n, err := sanitizeNodeForEncode(n); err != nil {
+	if n, err := sanitizeForEncode(n); err != nil {
 		return err
 	} else {
 		// DAG-CBOR is a superset of DAG-JOSE and can be used to encode valid DAG-JOSE objects. Use DAG-CBOR's Map sorting
@@ -75,42 +92,14 @@ func Encode(n datamodel.Node, w io.Writer) error {
 	}
 }
 
-func sanitizeNodeForEncode(n datamodel.Node) (datamodel.Node, error) {
-	rebuildRequired := false
-	// If `link` and `payload` are present, make sure they match.
-	if linkNode, err := n.LookupByString("link"); err != nil {
-		// It's ok for `link` to be absent (even if `payload` was present), but if some other error occurred,
-		// return it.
-		if _, linkNotFound := err.(datamodel.ErrNotExists); !linkNotFound {
-			return nil, err
-		}
-	} else {
-		// If `link` was present then `payload` must be present and the two must match. If any error occurs here
-		// (including `payload` being absent) return it.
-		payloadNode, err := n.LookupByString("payload")
-		if err != nil {
-			return nil, err
-		}
-		payloadString, err := payloadNode.AsString()
-		if err != nil {
-			return nil, err
-		}
-		cidFromPayload, err := cid.Decode(string(multibase.Base64url) + payloadString)
-		if err != nil {
-			return nil, err
-		}
-		linkFromNode, err := linkNode.AsLink()
-		if err != nil {
-			return nil, err
-		}
-		if linkFromNode.(cidlink.Link).Cid != cidFromPayload {
-			return nil, errors.New("cid mismatch")
-		}
-		// The node needs to be rebuilt without `link` before it can be encoded
-		rebuildRequired = true
-	}
-	// If the passed `Node` is not of type `_EncodedJOSE__Repr`, convert it to `_EncodedJOSE__Repr`.
-	if _, alreadyJose := n.(*_EncodedJOSE__Repr); !alreadyJose || rebuildRequired {
+func sanitizeForEncode(n datamodel.Node) (datamodel.Node, error) {
+	n, err := unflattenJOSE(n)
+	if err != nil {
+		return nil, err
+	} else if rebuildRequired, err := validateLinkBeforeEncode(n); err != nil {
+		return nil, err
+	} else if _, alreadyJose := n.(*_EncodedJOSE__Repr); !alreadyJose || rebuildRequired {
+		// If the passed `Node` is not of type `_EncodedJOSE__Repr`, convert it to `_EncodedJOSE__Repr`.
 		joseBuilder := Type.EncodedJOSE__Repr.NewBuilder().(*_EncodedJOSE__ReprBuilder)
 		// Copy the passed `Node` into `_EncodedJOSE__ReprBuilder`, which applies all the necessary validations required
 		// to construct a `_EncodedJOSE__Repr` node.
@@ -124,4 +113,41 @@ func sanitizeNodeForEncode(n datamodel.Node) (datamodel.Node, error) {
 		n = joseBuilder.Build().(schema.TypedNode).Representation()
 	}
 	return n, nil
+}
+
+func validateLinkBeforeEncode(n datamodel.Node) (bool, error) {
+	rebuildRequired := false
+	// If `link` and `payload` are present, make sure they match.
+	if linkNode, err := n.LookupByString("link"); err != nil {
+		// It's ok for `link` to be absent (even if `payload` was present), but if some other error occurred,
+		// return it.
+		if _, linkNotFound := err.(datamodel.ErrNotExists); !linkNotFound {
+			return false, err
+		}
+	} else {
+		// If `link` was present then `payload` must be present and the two must match. If any error occurs here
+		// (including `payload` being absent) return it.
+		payloadNode, err := n.LookupByString("payload")
+		if err != nil {
+			return false, err
+		}
+		payloadString, err := payloadNode.AsString()
+		if err != nil {
+			return false, err
+		}
+		cidFromPayload, err := cid.Decode(string(multibase.Base64url) + payloadString)
+		if err != nil {
+			return false, err
+		}
+		linkFromNode, err := linkNode.AsLink()
+		if err != nil {
+			return false, err
+		}
+		if linkFromNode.(cidlink.Link).Cid != cidFromPayload {
+			return false, errors.New("cid mismatch")
+		}
+		// The node needs to be rebuilt without `link` before it can be encoded
+		rebuildRequired = true
+	}
+	return rebuildRequired, nil
 }
